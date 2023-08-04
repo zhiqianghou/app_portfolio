47b3759663093e6aa364b63c09b6a97da63eb30a
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-08-04 16:34:01 -0500

    Add send emails using the form submit

--- send_email.py
+++ send_email.py
@@ -1,18 +1,15 @@
 import smtplib, ssl
 
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+	host = "smtp.gmail.com"
+	port = 465
+	username = "houzh.py@gmail.com"
+	password = "jzeoweetjbcjccxj"
+	receiver = "houzh.py@gmail.com"
+	context = ssl.create_default_context()
 
-username = "houzh.py@gmail.com"
-password = "jzeoweetjbcjccxj"
+	with smtplib.SMTP_SSL(host, port, context=context) as server:
+		server.login(username, password)
+		server.sendmail(username, receiver, message)
 
-receiver = "houzh.py@gmail.com"
-context = ssl.create_default_context()
-message = """\
-Subject: Hi!
-Test email sending and receiving from web app """
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-	server.login(username, password)
-	server.sendmail(username, receiver, message)
